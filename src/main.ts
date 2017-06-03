38a51561a744c425f7a587eacdef78941c231f7e
haimavni <haimavni@gmail.com>
2017-06-03 15:53:30 +0300

    froala still alive after installing dialog and validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import { Aurelia } from 'aurelia-framework'
 import environment from './environment';
+import * as Backend from 'i18next-xhr-backend';
+import { Router } from 'aurelia-router';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .plugin('aurelia-froala-editor')
+    .plugin('aurelia-i18n', (i18n) => {
+      i18n.i18next.use(Backend);
+
+      return i18n.setup({
+        backend: {
+          loadPath: './locales/{{lng}}/{{ns}}.json',
+        },
+        lng: 'he',
+        fallbackLng: 'he',
+        debug: environment.debug
+      }).then(() => {
+        const router = aurelia.container.get(Router);
+        const events = aurelia.container.get(EventAggregator);
+
+        router.transformTitle = title => i18n.tr(title);
+        events.subscribe('i18n:locale:changed', () => { router.updateTitle(); });
+      });
+    })
+    .plugin('aurelia-dialog')
+
     .feature('resources');
 
   if (environment.debug) {
