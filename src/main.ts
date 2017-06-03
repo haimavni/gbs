1c19553c15cb6d608cd699cd367c75096b0e90a9
haimavni <haimavni@gmail.com>
2017-06-03 12:14:45 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { Aurelia } from 'aurelia-framework'
+import environment from './environment';
+
+export function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .plugin('aurelia-froala-editor')
+    .feature('resources');
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
+  aurelia.start().then(() => aurelia.setRoot());
+}
