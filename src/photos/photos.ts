7c477e006752f9354d3e212c4fb842bb18ec3ecb
haimavni <haimavni@gmail.com>
2017-06-30 18:22:32 +0300

    wip

--- src/photos/photos.ts
+++ src/photos/photos.ts
@@ -1,3 +1,25 @@
-export class Home {
+export class Photos {
+    filter = "";
+    photos_per_line = 8;
+    photo_size = 128;
+    url = "http://gbstories:8000/gbs/static/gb_photos/gbs/photos/orig/micha2/Ppm0244.jpg";
+    photo_list = [];
+    photos_margin=0;
     
+
+    constructor() {
+        for (let i = 0; i < 250; i++) {
+            let photo = { src: this.url };
+            this.photo_list.push(photo);
+        }
+    }
+
+    slider_changed() {
+        let width = document.getElementById("photos-container").offsetWidth;
+        this.photo_size = Math.floor((width - 60) / this.photos_per_line); 
+        console.log("slider now at ", this.photos_per_line);
+        console.log("photo_size: ", this.photo_size );
+        this.photo_list = this.photo_list.splice(0);
+    }
+
 }
