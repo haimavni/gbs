f91426fc1bedff0cd5946d329c8a94bbf2098adb
haimavni <haimavni@gmail.com>
2017-07-21 00:14:06 +0300

    wip

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,21 +1,24 @@
 import { MemberGateway } from '../services/gateway';
 import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 
 @autoinject
 export class Home {
     api;
     photo_list;
     video_list = [{ type: "youtube", src: "//www.youtube.com/embed/dfJIOa6eyfg?wmode=opaque" },
-                  { type: "youtube", src: "https://www.youtube.com/embed/cscYO3epaIY?wmode=opaque" }];
-    member_of_the_day = {gender: '', name: ''};
+    { type: "youtube", src: "https://www.youtube.com/embed/cscYO3epaIY?wmode=opaque" }];
+    member_of_the_day = { gender: '', name: '' };
     member_prefix;
     was_born_in;
     died_in;
+    router;
 
-    constructor(api: MemberGateway) {
+    constructor(api: MemberGateway, router: Router) {
         console.log("at home. constructor.")
         this.api = api;
         this.photo_list = this.api.call_server_post('members/get_photo_list');
+        this.router = router;
     }
 
     action(slide, event) {
@@ -24,11 +27,15 @@ export class Home {
 
     attached() {
         this.api.call_server_post('members/get_random_member').then(result => {
-            this.member_of_the_day=result.member_data;
+            this.member_of_the_day = result.member_data;
             this.member_prefix = this.member_of_the_day.gender == 'F' ? 'home.female-member-of-the-day' : 'home.male-member-of-the-day';
             this.was_born_in = this.member_of_the_day.gender == 'F' ? 'home.female-was-born-in' : 'home.male-was-born-in';
             this.died_in = this.member_of_the_day.gender == 'F' ? 'home.female-died-in' : 'home.male-died-in';
         });
+    }
+
+    jump_to_member_of_the_day_page(member_id) {
+        this.router.navigateToRoute('member-details', { id: member_id });
     }
 
 }
