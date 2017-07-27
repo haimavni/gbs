b1ae14a8ea12772d36279551c74e826a05c6e4d4
haimavni <haimavni@gmail.com>
2017-07-28 01:50:15 +0300

    wip

--- src/resources/elements/partial-date.ts
+++ src/resources/elements/partial-date.ts
@@ -2,18 +2,12 @@ import { bindable, inject, DOM, computedFrom } from 'aurelia-framework';
 
 @inject(DOM.Element)
 export class PartialDateCustomElement {
-    @bindable century = '19';
-    @bindable decade = '2';
+    @bindable decade = '192';
     @bindable year = '8';
     @bindable month = '07';
     @bindable day = '17';
     @bindable date_str: string;
     element;
-    centuries = ["18", "19", "20"]; //for debugging. remove soon.
-    decades = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    years = ["?", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    months = ["??", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-    days = ["??", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "34", "25", "26", "27", "28", "29", "30", "31"];
 
     constructor(element, user, dialog) {
         this.element = element;
@@ -32,33 +26,21 @@ export class PartialDateCustomElement {
             date_str = date_str + "-??-??";
         }
         let lst = date_str.split('-');
-        this.century = lst[0].substring(0, 3);
-        this.decade = lst[0].substring(3, 4);
+        this.decade = lst[0].substring(0, 3);
+        this.year = lst[0].substring(3,4);
         this.month = lst[1];
         this.day = lst[2];
     }
 
-    //@computedFrom('century', 'decade', 'year', 'month', 'day')
     update_date_str() {
-        this.date_str = this.century + this.decade + this.year + "-" + this.month + "-" + this.day;
-    }
-
-    centuryChanged(newValue, oldValue) {
-        if (newValue.includes("?")) {
-            this.date_str = "????-??-??";
-            return;
-        }
-        if (this.century.length == 2) {
-            this.focus("decade");
-        }
-        this.update_date_str();
+        this.date_str = this.decade + this.year + "-" + this.month + "-" + this.day;
     }
 
     decadeChanged(newValue, oldValue) {
-        if (this.decade.length == 1) {
+        if (this.decade.length == 3) {
             this.focus("year");
+            this.update_date_str();
         }
-        this.update_date_str();
     }
 
     yearChanged(newValue, oldValue) {
@@ -70,9 +52,9 @@ export class PartialDateCustomElement {
             this.month = "??";
         }
         if (this.month.length == 2) {
-            this.focus("century");
-        }
+            this.focus("decade");
         this.update_date_str();
+        }
     }
 
     dayChanged(newValue, oldValue) {
@@ -81,8 +63,8 @@ export class PartialDateCustomElement {
         }
         if (this.day.length == 2) {
             this.focus("month");
-        }
         this.update_date_str();
+        }
     }
 
     date_strChanged(newValue, oldValue) {
@@ -92,8 +74,7 @@ export class PartialDateCustomElement {
     split_date_str() {
         if (this.date_str) {
             let lst = this.date_str.split('-');
-            this.century = lst[0].substring(0, 2);
-            this.decade = lst[0].substring(2, 3);
+            this.decade = lst[0].substring(0, 3);
             this.year = lst[0].substring(3, 4);
             this.month = lst[1];
             this.day = lst[2];
