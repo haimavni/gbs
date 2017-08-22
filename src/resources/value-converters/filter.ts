a6058270e955d83e4c8623f6c4f043ef5dedc23b
haimavni <haimavni@gmail.com>
2017-08-22 18:13:45 +0300

    wip

--- src/resources/value-converters/filter.ts
+++ src/resources/value-converters/filter.ts
@@ -1,29 +1,27 @@
 export class FilterValueConverter {
-  toView(array, value, max=24, ...properties) {
+  toView(array, value, ...properties) {
     if (!array) {
       return [];
     }
     value = (value || '').trim().toLowerCase();
-    
+
     if (!value) {
-      return array.slice(0, max);
+      return array.slice(0);
     }
     let parts = value.toLowerCase().split(" ");
-    let arr = array.filter(item => 
-      parts.every(part => properties.some(function(this, property, index, properties) {
+    let arr = array.filter(item =>
+      parts.every(part => properties.some(function (this, property, index, properties) {
         let p = (item[property] || "").toLowerCase();
         if (part.startsWith("[") || part.startsWith("]")) {
           p = part[0] + p;
         }
         if (part.endsWith("[") || part.endsWith("]")) {
-          p = p + part[part.length-1]; 
+          p = p + part[part.length - 1];
         }
         return p.includes(part);
       })
-      //properties.some(property => 
-        //(item[property] || '').toLowerCase().includes(part))
-        ));
-    arr = arr.slice(0, max);
+      ));
+    arr = arr.slice(0);
     return arr;
   }
 }
