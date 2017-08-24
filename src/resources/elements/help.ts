328c7ab2b5c535b810b63e8c4e860dc36d4dd5b2
haimavni <haimavni@gmail.com>
2017-08-24 16:17:13 +0300

    help

--- src/resources/elements/help.ts
+++ src/resources/elements/help.ts
@@ -1,6 +1,9 @@
 import { bindable, autoinject, singleton } from 'aurelia-framework';
 import { User } from '../../services/user';
 import { MemberGateway } from '../../services/gateway';
+import { StoryWindow } from '../../stories/story_window';
+import { DialogService } from 'aurelia-dialog';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
 @autoinject()
 @singleton()
@@ -9,20 +12,37 @@ export class HelpCustomElement {
     user;
     api;
     @bindable position = 'top';
-    title = "Title";
-    content = ""
+    story_info;
+    dialog;
+    eventAggregator;
 
-    constructor(user: User, api: MemberGateway) {
+    constructor(user: User, api: MemberGateway, dialog: DialogService, eventAggregator: EventAggregator) {
         this.user = user;
         this.api = api;
+        this.dialog = dialog;
+        this.eventAggregator = eventAggregator;
+        this.eventAggregator.subscribe('EditModeChange', payload => this.refresh())
+    }
+
+    refresh() {
+        let topic = this.topic;
+        if (this.user.editing ) {
+            topic += '__edit';
+        }
+        this.api.call_server('help/get_help', { topic: topic })
+            .then(response => {
+                this.story_info = response.story_info;
+            })
     }
 
     attached() {
-        this.api.call_server('help/get_help', {topic: this.topic})
-        .then(response => {
-            this.title = response.title;
-            this.content = response.content;
-        })
+        this.refresh();
+    }
 
+    edit_help_message() {
+        if (!this.user.privileges.HELP_AUTHOR) return;
+        this.dialog.open({ viewModel: StoryWindow, model: { story: this.story_info, edit: true }, lock: true }).whenClosed(response => {
+            //console.log("response after edit dialog: ", response.output);
+        });
     }
 }
