8d85b7deb743e7db66d52e5d3d9f334af95167c9
haimavni <haimavni@gmail.com>
2017-08-31 01:01:07 +0300

    wip

--- src/resources/value-converters/filter-by-set.ts
+++ src/resources/value-converters/filter-by-set.ts
@@ -1,12 +1,13 @@
 export class FilterBySetValueConverter {
-    toView(array, value) {
+    toView(array, value, exc_inc) {
+        let bool = (exc_inc == 'include');
         if (!array) {
             return [];
         }
         if (! value) {
             return array;
         }
-        let arr = array.filter(option => !value.has(option.name));
+        let arr = array.filter(option => value.has(option.name) == bool);
         return arr;
     }
 }
