ffc6b7a4b630e6d4ebfe9ebde76452b8df396c3c
haimavni <haimavni@gmail.com>
2018-02-20 21:33:30 +0200

    start multi lingual

---
+++ src/resources/elements/locale-picker.ts
@@ -0,0 +1,27 @@
+import {autoinject, bindable} from 'aurelia-framework';
+import {I18N} from 'aurelia-i18n';
+
+@autoinject
+export class LocalePickerCustomElement {
+ 
+  @bindable selectedLocale;
+  @bindable locales = ['en', 'he'];
+  i18n;
+  isChangingLocale = false;
+
+  constructor(i18n: I18N) {
+    this.i18n = i18n;
+
+    this.selectedLocale = this.i18n.getLocale();
+    this.isChangingLocale = false;
+    let x = this.i18n.locales;
+    alert('locales: ' + x);
+  }
+
+  selectedLocaleChanged() {
+    this.isChangingLocale = true;
+    this.i18n.setLocale(this.selectedLocale).then(() => {
+      this.isChangingLocale = false;
+    });
+  }
+}
