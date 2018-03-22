b9110c3793cbea918c32a5a00b70a771bd5d9dbf
haimavni <haimavni@gmail.com>
2018-03-22 16:58:43 +0200

    death cause

--- src/resources/elements/selector.ts
+++ src/resources/elements/selector.ts
@@ -1,29 +1,44 @@
-import { bindable, inject, DOM, bindingMode } from 'aurelia-framework';
+import { bindable, inject, DOM, bindingMode, computedFrom } from 'aurelia-framework';
 import { I18N } from 'aurelia-i18n';
+import { User } from '../../services/user';
 
-@inject(DOM.Element, I18N)
+@inject(DOM.Element, I18N, User)
 export class SelectorCustomElement {
     @bindable dict;
     @bindable selectedval;
+    @bindable suffix;
     selected;
     options;
     i18n;
+    user;
     element;
 
-    constructor(element, i18n) {
+    constructor(element, i18n, user) {
         this.element = element;
         this.i18n = i18n;
+        this.user = user;
     }
 
     attached() {
         let keys = Object.keys(this.dict);
+        let suffix = this.suffix ? '-' + this.suffix : '';
         this.options = [];
         for (let key of keys) {
-            let name = 'consts.' + key.replace(/_/g, '-').toLowerCase();
+            let name = 'consts.' + (key.replace(/_/g, '-') + suffix).toLowerCase();
             let selected = { name: this.i18n.tr(name), value: this.dict[key] }
             this.options.push(selected);
         }
         this.options.sort((option1, option2) => option1.value - option2.value);
+    }
+
+    @computedFrom('selectedval')
+    get display() {
+        let selected = this.options.find(opt=>opt.value==this.selectedval);
+        if (selected) {
+            return selected.name;
+        } else {
+            return "";
+        }
     }
 
     selectedvalChanged(newValue, oldValue) {
