6b48471a99d390871f7a27800007cad30f854c96
haimavni <haimavni@gmail.com>
2018-03-26 13:48:15 +0300

    watch version. use toastr.

--- src/services/watch_version.ts
+++ src/services/watch_version.ts
@@ -1,10 +1,10 @@
 import environment from '../environment';
 import { autoinject, singleton, noView } from "aurelia-framework";
 import { MemberGateway } from './gateway';
-import { DialogService } from 'aurelia-dialog';
 import { I18N } from 'aurelia-i18n';
+import * as toastr from 'toastr';
 
-let API;
+let THIS1;
 
 @autoinject()
 @singleton()
@@ -12,37 +12,34 @@ let API;
 export class WatchVersion {
     api;
     nudnik;
-    user_warned = false;
-    dialog;
+    user_warned = 0;
     i18n;
 
-    constructor(api: MemberGateway, dialog: DialogService, i18n: I18N) {
+    constructor(api: MemberGateway, i18n: I18N) {
         this.api = api;
-        API = this.api;
-        console.log('this api: ', this.api);
+        THIS1 = this;
         this.verify_latest_version();
-        this.nudnik = setInterval(this.verify_latest_version, 10000);
-        this.dialog = dialog;
+        this.nudnik = setInterval(this.verify_latest_version, 60000);
         this.i18n = i18n;
     }
 
     verify_latest_version() {
         let enversion = environment.version;
-        console.log("enter verify. enversion: ", enversion);
         if (!enversion) {
             return
         }
-        console.log("api: ", this.api, " API: ", API);
-        API.call_server('default/get_curr_version')
+        THIS1.api.call_server('default/get_curr_version')
             .then(response => {
-                console.log("curr version: ", response);
                 if (enversion && environment.version != response.version) {
-                    alert("Please refresh! Your version is " + enversion + ". Current version: " + response.version);
-                    if (!this.user_warned) {
-                        clearInterval(this.nudnik);
-                        this.nudnik = setInterval(this.verify_latest_version, 3600000);
+                    let msg_head = THIS1.i18n.tr('please-update-head');
+                    let msg_body = THIS1.i18n.tr('please-update-body');
+                    let msg_tail = THIS1.user_warned ? THIS1.i18n.tr('please-update-stubborn') : "";
+                    toastr.warning(msg_body + msg_tail, msg_head, {timeOut: 60000});
+                    if (THIS1.user_warned > 1) {
+                        clearInterval(THIS1.nudnik);
+                        THIS1.nudnik = setInterval(THIS1.verify_latest_version, 600000);
                     }
-                    this.user_warned = true;
+                    THIS1.user_warned += 1;
                 }
             })
     }
