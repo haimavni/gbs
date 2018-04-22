d7cf5713824c53b71bb518baa89d9660b27ad54f
haimavni <haimavni@gmail.com>
2018-04-22 23:49:45 +0300

    ad hoc scripts implemented.

--- src/services/show-json.ts
+++ src/services/show-json.ts
@@ -2,12 +2,8 @@ import * as JsonHuman from 'json.human';
 
 export class HumanJson {
 
-    test() {
-        let x = { name: "haim", last_name: "avni" };
-
-        let node = JsonHuman.format(x);
-        console.log("json human node: ", node);
-        console.log("inner html: ", node.innerHTML);
+    display(obj) {
+        let node = JsonHuman.format(obj);
         return node;
     }
 }
