f559d103cb3f27de4f243dde66e9f6f22ccf8363
haimavni <haimavni@gmail.com>
2018-04-25 23:35:19 +0300

    typo fixed

--- src/photos/photos.ts
+++ src/photos/photos.ts
@@ -345,7 +345,7 @@ export class Photos {
     delete_selected_photos() {
         this.api.call_server_post('members/delete_selected_photos', this.params)
             .then(() => {
-                this.params.selected_photo_list = []];
+                this.params.selected_photo_list = [];
                 this.selected_photos = new Set();
                 this.update_photo_list();
             });
