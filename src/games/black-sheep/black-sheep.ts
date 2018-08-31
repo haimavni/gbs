9740450d6fe35d6e034116e78d39a5d548ef2548
haimavni <haimavni@gmail.com>
2018-09-01 02:55:04 +0300

    black sheep works

--- src/games/black-sheep/black-sheep.ts
+++ src/games/black-sheep/black-sheep.ts
@@ -1,14 +1,25 @@
+import { MemberGateway } from '../../services/gateway';
+import { autoinject } from 'aurelia-framework';
+
+@autoinject
 export class BlackSheep {
+    api;
     black_loc = 12;
-    white_locs = [0,1,2,100,101,11,110,111,12,21,22];
+    white_locs = new Set([0, 1, 2, 100, 101, 11, 110, 111, 12, 21, 22]);
     mode = 'placing'; // solving-display
+    solution_steps = [];
+    curr_step_idx = 0;
+    failed = false;
+    
+    constructor(api: MemberGateway, ) {
+        this.api = api;
+    }
 
     status(r, c, m) {
         let n = 100 * m + 10 * r + c;
-        let kind = m ? 'middle' : 'main';
-        if (this.black_loc == n) return 'black ' + kind;
-        if (this.white_locs.findIndex(item => item == n) >= 0) return 'white ' + kind;
-        return 'empty ' + kind;
+        if (this.black_loc == n) return 'black';
+        if (this.white_locs.has(n)) return 'white';
+        return 'lightgray';
     }
 
     has_border(r, c) {
@@ -18,7 +29,83 @@ export class BlackSheep {
 
     new_game() {
         this.black_loc = -1;
-        this.white_locs = [];
+        this.white_locs = new Set([]);
+        this.curr_step_idx = 0;
+        this.solution_steps = [];
+        this.refresh();
+        this.failed = false;
     }
 
+    mutate(r, c, m, event) {
+        console.log("mutate. event: ", event);
+        let n = 100 * m + 10 * r + c;
+        if (this.black_loc == n) {
+            this.black_loc = -1
+        }
+        else if (this.white_locs.has(n)) {
+            this.white_locs.delete(n);
+            this.black_loc = n;
+        } else {
+            this.white_locs.add(n);
+        }
+        event.target.style.backgroundColor = this.status(r, c, m);
+    }
+
+    refresh() {
+        for (let loc of [0, 1, 2, 3, 10, 11, 12, 20, 21, 22, 100, 101, 110, 111]) {
+            let locs = '' + loc;
+            let el = document.getElementById(locs)
+            let c = loc % 10;
+            let r = (loc - c) / 10;
+            let m = (loc - c - r * 10) / 100;
+            if (el) {
+                el.style.backgroundColor = this.status(r, c, m);
+            }
+        }
+    }
+
+    id_of_loc(r, c, m) {
+        return 100 * m + 10 * r + c;
+    }
+
+    solve() {
+        let arr = Array.from(this.white_locs);
+        this.api.call_server_post('games/solve_black_sheep', { black_loc: this.black_loc, white_locs: arr })
+            .then(response => {
+                this.solution_steps = response.solution_steps;
+                this.failed = this.solution_steps.length == 0;
+            })
+    }
+
+    get has_solution() {
+        return this.solution_steps.length > 0;
+    }
+
+    next() {
+        let step = this.solution_steps[this.curr_step_idx];
+        this.curr_step_idx += 1;
+        this.white_locs.delete(step[1]);
+        let black = step[0];
+        if (black) {
+            this.black_loc = step[3]
+        } else {
+            this.white_locs.delete(step[2]);
+            this.white_locs.add(step[3]);
+        }
+        this.refresh();
+    }
+
+    prev() {
+        this.curr_step_idx -= 1;
+        let step = this.solution_steps[this.curr_step_idx];
+        this.white_locs.add(step[1]);
+        let black = step[0];
+        if (black) {
+            this.black_loc = step[2]
+        } else {
+            this.white_locs.delete(step[3]);
+            this.white_locs.add(step[2]);
+        }
+        this.refresh();
+    }
 }
