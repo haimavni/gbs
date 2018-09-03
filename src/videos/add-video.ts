6ba34263ad63e460926b413db4b55ccd10e32e80
haimavni <haimavni@gmail.com>
2018-09-04 01:18:58 +0300

    videos. wip.

---
+++ src/videos/add-video.ts
@@ -0,0 +1,51 @@
+import { DialogController } from 'aurelia-dialog';
+import { I18N } from 'aurelia-i18n';
+import { autoinject, computedFrom } from 'aurelia-framework';
+import { MemberGateway } from '../services/gateway';
+import { Theme } from "../services/theme";
+import { EventAggregator } from 'aurelia-event-aggregator';
+import * as toastr from 'toastr';
+import { User } from '../services/user';
+
+@autoinject()
+export class AddVideo {
+    controller;
+    api;
+    theme;
+    user;
+    i18n;
+    header_text;
+    params = {
+        video_address: '',
+        video_name: '',
+        user_id: null
+    }
+
+    constructor(controller: DialogController, api: MemberGateway, theme: Theme, i18n: I18N, user: User) {
+        this.controller = controller;
+        this.api = api;
+        this.theme = theme;
+        this.i18n = i18n;
+        this.header_text = 'videos.header-text';
+        this.user = user;
+    }
+
+    send() {
+        this.params.user_id = this.user.id;
+        this.api.call_server_post('members/save_video', this.params)
+            .then(() => {
+                toastr.success("<p dir='rtl'>" + this.i18n.tr('video.message-successful') + "</p>", '', 6000);
+                this.controller.ok();
+            });
+    }
+
+    cancel() {
+        this.controller.cancel();
+    }
+
+    @computedFrom('params.video_address', 'params.video_name')
+    get is_disabled() {
+        return (this.params.video_address=='' || this.params.video_name=='');
+    }
+
+}
--- src/videos/videos.ts
+++ src/videos/videos.ts
@@ -4,6 +4,8 @@ import { Theme } from "../services/theme";
 import { autoinject, singleton } from 'aurelia-framework';
 import { I18N } from 'aurelia-i18n';
 import { Router } from 'aurelia-router';
+import { DialogService } from 'aurelia-dialog';
+import { AddVideo } from './add-video';
 
 @autoinject
 @singleton()
@@ -17,20 +19,21 @@ export class Videos {
     router;
     scroll_area;
     scroll_top = 0;
+    dialog;
 
-    constructor(api: MemberGateway, user: User, i18n: I18N, theme: Theme, router: Router) {
+    constructor(api: MemberGateway, user: User, i18n: I18N, theme: Theme, router: Router, dialog: DialogService) {
         this.api = api;
         this.user = user;
         this.i18n = i18n;
         this.theme = theme;
-        this.router = router;
+        this.dialog = dialog;
         this.api.call_server_post('members/get_video_sample')
             .then(response => this.set_video_list(response.video_list));
     }
 
     attached() {
         this.theme.display_header_background = true;
-        this.theme.page_title = "videos.videos";
+        this.theme.page_title = "videos.video-clips";
         this.scroll_area.scrollTop = this.scroll_top;
     }
 
@@ -40,7 +43,15 @@ export class Videos {
     }
 
     new_video() {
-
+        this.theme.hide_title = true;
+        this.dialog.open({ viewModel: AddVideo, lock: true }).whenClosed(response => {
+            this.theme.hide_title = false;
+            if (!response.wasCancelled) {
+                //do something?
+            } else {
+                //do something else?
+            }
+        });
     }
 
     set_video_list(video_list) {
@@ -48,7 +59,7 @@ export class Videos {
     }
 
     youtube_data(video_code) {
-        return { type: "youtube", src: "//www.youtube.com/embed/" + video_code + "?wmode=opaque" }
+        return { type: "youtube", src: "//www.youtube.com/embed/" + video_code + "?wmode=opaque", name: video_code, selected: false }
     }
 
 }
