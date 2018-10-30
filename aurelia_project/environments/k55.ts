38cdb218d1888b77aec921ed3260f3ea30178002
haimavni <haimavni@gmail.com>
2018-10-30 11:48:39 +0200

    allow no logo

--- aurelia_project/environments/k55.ts
+++ aurelia_project/environments/k55.ts
@@ -1,7 +1,7 @@
 export default {
     debug: true,
     testing: false,
-    baseURL: "http://gbstories:8000",
+    baseURL: "http://gbstories.com:8000",
     app: 'kesher55',
     version: "",
     i18n_ver: ''
