e06557ad770139c74d26e443e25185ed8500dbe9
haimavni <haimavni@gmail.com>
2020-04-08 01:04:21 +0300

    update topics of a single photo

--- src/photos/photo-detail.ts
+++ src/photos/photo-detail.ts
@@ -41,6 +41,14 @@ export class PhotoDetail {
     photographers_settings;
     topic_list = [];
     photographer_list = [];
+    has_grouped_topics = false;
+    topic_groups = [];
+    photo_topics;
+    params = {
+        selected_topics: [],
+        selected_photographers: [],
+        photo_ids: [],
+    };
 
     constructor(api: MemberGateway, i18n: I18N, user: User, dialog: DialogService, router: Router) {
         this.api = api;
@@ -53,7 +61,6 @@ export class PhotoDetail {
             can_set_sign: true,
             can_group: true,
             empty_list_message: this.i18n.tr('photos.no-topics-yet'),
-            show_untagged: this.user.editing
         });
         this.photographers_settings = new MultiSelectSettings({
             clear_filter_after_select: true,
@@ -73,7 +80,8 @@ export class PhotoDetail {
                 this.photo_story = response.photo_story;
                 this.photo_name = this.photo_story.name || response.photo_name;
                 this.photographer_name = response.photographer_name;
-                this.keywords = response.keywords;
+                this.photo_topics = response.photo_topics;
+                this.topic_list = response.topic_list;
                 this.true_photo_id = response.photo_id; //this.photo_id may be associated story id
                 if (this.photo_story.story_id == 'new') {
                     this.photo_story.name = this.i18n.tr('photos.new-story');
@@ -84,12 +92,32 @@ export class PhotoDetail {
                 this.orig_photo_width = response.width;
                 this.orig_photo_height = response.height;
                 this.chatroom_id = response.chatroom_id;
+                this.init_selected_topics();
                 this.calc_photo_width();
             });
             this.update_topic_list();
     }
 
-    calc_photo_width() {
+    init_selected_topics() {
+        this.params.selected_topics = [];
+        let i = 0;
+        for (let opt of this.photo_topics) {
+            opt.sign = 'plus';
+            let itm = {option: opt, first: i == 0, last: i == this.photo_topics.length - 1}
+            this.params.selected_topics.push(itm);
+            i += 1;
+        }
+    }
+
+    @computedFrom('photo_topics')
+    get topic_names() {
+        if (!this.photo_topics) return "";
+        let topic_name_list = this.photo_topics.map(itm => itm.name);
+        return topic_name_list.join(';');
+    }
+
+    async calc_photo_width() {
+        console.log("tinofet 1");
         let pw = this.orig_photo_width / this.MAX_WIDTH;
         let ph = this.orig_photo_height / this.MAX_HEIGHT;
         if (pw >= ph) {
@@ -97,7 +125,10 @@ export class PhotoDetail {
         } else {
             this.photo_width = this.orig_photo_width / ph;
         }
+        console.log("tinofet 2");
+        await sleep(100);
         let el = document.getElementById('photo-box');
+        console.log("el is: ", el);
         let width = el.clientWidth;
         el.style.paddingRight = `${width - this.photo_width - 15}px`;
     }
@@ -170,13 +201,29 @@ export class PhotoDetail {
     }
 
     update_topic_list() {
-        this.api.call_server('topics/get_topic_list', 'P')
+        this.api.call_server_post('topics/get_topic_list', {usage: 'P'})
             .then(result => {
                 this.topic_list = result.topic_list;
-                //this.topic_groups = result.topic_groups;
+                this.topic_groups = result.topic_groups;
                 this.photographer_list = result.photographer_list;
             });
     }
 
+    handle_topic_change(event) {
+        if (!event.detail) return;
+        this.params.selected_topics = event.detail.selected_options
+        let topics = this.params.selected_topics.map(top => top.option);
+        this.photo_topics = topics;
+        this.api.call_server_post('photos/apply_topics_to_photo', {photo_id: this.true_photo_id, topics: this.photo_topics});
+    }
+
+    handle_photographer_change(event) {
+        console.log(" topic change. event detail: ", event.detail, " selected photographers: ", this.params.selected_photographers);
+        this.params.selected_photographers = event.detail.selected_options;
+    }
+
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
