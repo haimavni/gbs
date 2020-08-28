297eb17dad371babc421fd3e5c16fbe54e75491a
haimavni <haimavni@gmail.com>
2020-08-28 11:47:06 +0300

    "no title" only if new story

--- src/stories/story-detail.ts
+++ src/stories/story-detail.ts
@@ -320,7 +320,7 @@ export class StoryDetail {
     @computedFrom('story')
     get story_name() {
         if (this.story) return this.story.name;
-        return this.i18n.tr('stories.new-story');
+        return '';
     }
 
     @computedFrom('story_idx')
