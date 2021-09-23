18740cc16606908e11f10bc673e1ead4eee68dc9
haimavni <haimavni@gmail.com>
2021-09-23 14:28:48 +0300

    point gbstories env to master

--- aurelia_project/environments/gbstories.ts
+++ aurelia_project/environments/gbstories.ts
@@ -1,7 +1,7 @@
 export default {
     debug: true,
     testing: false,
-    baseURL: "https://tol.life/gbs__test",
+    baseURL: "https://tol.life/gbs__master",
     app: '',
     version: "",
     i18n_ver: ''
