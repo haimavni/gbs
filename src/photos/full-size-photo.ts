c0d354d43689c3b38c8d5751109d72aa7822d42c
haimavni <haimavni@gmail.com>
2021-12-22 00:48:42 +0200

    again

--- src/photos/full-size-photo.ts
+++ src/photos/full-size-photo.ts
@@ -713,6 +713,8 @@ export class FullSizePhoto {
             });
         let href = `https://facebook.com/sharer/sharer.php?u=${card_url}&t=${title}`;
         await this.loadImage(img_src, img_elem);
+        await sleep(5000);
+        console.log("popup now");
         this.popup.popup('SHARER', href, "height=600,width=800,left=200,top=100");
         await sleep(5000);
     }
