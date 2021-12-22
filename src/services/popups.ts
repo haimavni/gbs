7fb6c08c4e1ba7a0d727adcdc9c0a554d4d5530d
haimavni <haimavni@gmail.com>
2021-12-22 12:31:33 +0200

    another experiment

--- src/services/popups.ts
+++ src/services/popups.ts
@@ -1,10 +1,20 @@
+import { Misc } from '../services/misc';
+import { autoinject} from 'aurelia-framework';
+
+@autoinject()
 export class Popup {
     popupWindows = {};
+    misc: Misc;
 
-    popup(key, url, params) {
+    constructor(misc: Misc) {
+        this.misc = misc;
+    }
+
+    async popup(key, url, params) {
         let hnd = this.popupWindows[key];
         if (hnd) {
             hnd.close();
+            await this.misc.sleep(2000);
         }
         let w = this.popupWindows[key] = window.open(url, '_blank', params);
         let d = w.document;
