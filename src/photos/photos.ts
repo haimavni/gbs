b286f57540dadf62df1bfacc345af815d9775478
haimavni <haimavni@gmail.com>
2022-01-06 17:19:53 +0200

    still error

--- src/photos/photos.ts
+++ src/photos/photos.ts
@@ -374,7 +374,7 @@ export class Photos {
             event.stopPropagation();
             //this.openDialog(slide);
             let photo_ids = this.photo_list.map(photo => photo.photo_id);
-            photo_ids = photo_ids.slice(0, 800); //to prevent server errors such as "invalid gateway"
+            photo_ids = photo_ids.slice(0, 100); //to prevent server errors such as "invalid gateway"
             this.router.navigateToRoute('photo-detail', {
                 id: slide.photo_id,
                 keywords: "",
