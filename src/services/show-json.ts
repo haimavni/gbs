5d8e7d028a85c08555e29b0aa3c01f4401dbad04
haimavni <haimavn@gmail.com>
2022-09-28 18:02:25 +0300

    wip

--- src/services/show-json.ts
+++ src/services/show-json.ts
@@ -3,7 +3,6 @@ import * as JsonHuman from 'json.human';
 export class HumanJson {
 
     display(obj) {
-        console.log("human json. obj: ", obj);
         let node = JsonHuman.format(obj);
         return node;
     }
