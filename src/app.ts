4b18bdc41fd7d6c1c7dbaea15c74059e129995df
haimavni <haimavn@gmail.com>
2022-10-01 21:05:30 +0300

    wip

--- src/app.ts
+++ src/app.ts
@@ -59,7 +59,7 @@ export class App {
     async configureRouter(config, router) {
         router.title = 'app-title';
         config.options.pushState = true;
-        //config.options.root = '/';
+        config.options.root = '/';
         for (let i=0; i < 100; i+=1) {
             if (this.user.config_ready) break;
             await this.misc.sleep(100);
