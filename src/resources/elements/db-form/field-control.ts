da76a7f452f4890cfaaebef8f1adf030ae1355a9
haimavni <haimavn@gmail.com>
2022-12-01 11:12:34 +0200

    db form - wip

--- src/resources/elements/db-form/field-control.ts
+++ src/resources/elements/db-form/field-control.ts
@@ -1,6 +1,21 @@
-import { FieldInfo } from './field-info';
+import { bindable, inject, DOM, bindingMode, computedFrom } from 'aurelia-framework';
+import { DialogService } from 'aurelia-dialog';
+import { I18N } from 'aurelia-i18n';
+import { User } from '../../../services/user';
+import { Theme } from '../../../services/theme';
+import { Misc } from '../../../services/misc';
 
+enum ui_kind {
+    F_SELECT,
+    F_BOOLEAN,
+    F_INTEGER,
+    F_DATE
+}
 export class FieldControl {
-    field_info: FieldInfo;
+    @bindable name = '';
+    @bindable type = '';
+    @bindable description = '';
+    @bindable options = [];
+
 
 }
