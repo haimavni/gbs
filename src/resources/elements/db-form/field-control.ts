d48acd698bf85b6ee772c390443ef3f5504be6bc
haimavni <haimavn@gmail.com>
2022-12-06 13:07:17 +0200

    field control - wip

--- src/resources/elements/db-form/field-control.ts
+++ src/resources/elements/db-form/field-control.ts
@@ -5,39 +5,65 @@ import { User } from '../../../services/user';
 import { Theme } from '../../../services/theme';
 import { Misc } from '../../../services/misc';
 
-export enum ui_kind {
-    F_STRING,
-    F_SELECT,
-    F_BOOLEAN,
-    F_INTEGER,
-    F_DATE
+export enum data_type {
+    F_STRING='string',
+    F_BOOLEAN = 'boolean',
+    F_INTEGER = 'integer',
+    F_DATE = 'date'
 }
+
+@inject(User, Theme, DOM.Element, I18N)
 export class FieldControl {
     i18n: I18N;
+    user: User;
+    theme: Theme;
     @bindable name = '';
-    @bindable type: ui_kind;
+    @bindable type: data_type;
     @bindable description = '';
     @bindable options = [];
+    element;
     ui_selector = '';
+    int_val = 0;
+    bool_val = false;
+    str_val = '';
+    date_val = '01.01.01'
 
-    constructor(i18n: I18N) {
+    constructor(user: User, theme: Theme, element, i18n) {
         this.i18n = i18n;
+        this.user = user;
+        this.theme = theme;
+        this.element = element;
     }
 
-    get ui_kind() {
-        switch(this.type) {
-            case ui_kind.F_STRING:
-                return 'string';
-            case ui_kind.F_SELECT:
-                return 'select';
-            case ui_kind.F_BOOLEAN:
-                if (this.options.length == 2)
-                    return 'bool-opt';
-                return 'bool';
-            case ui_kind.F_INTEGER:
-                return 'integer';
-            case ui_kind.F_DATE:
-                return 'date';
+    get data_type() {
+        let result = this.data_type;
+        if (this.options.length > 1) {
+            result += '-select'
         }
+        return result;
+    }
+    dispatch_event() {
+        let data: any;
+        switch(this.type) {
+            case 'string':
+                data = this.str_val;
+                break;
+            case 'boolean':
+                data = this.bool_val;
+                break;
+            case 'integer':
+                data = this.int_val;
+                break;
+            case 'date':
+                data = this.date_val;
+                break;
+        }
+        let changeEvent = new CustomEvent('change', {
+            detail: {
+                data: data
+            },
+            bubbles: true
+        });
+        this.element.dispatchEvent(changeEvent);
     }
 }
