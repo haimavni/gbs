e1434009c2316436240a8bc77c27dc36d2c4a218
haimavni <haimavn@gmail.com>
2022-12-18 13:10:19 +0200

    wip

--- src/experiments/experiment.ts
+++ src/experiments/experiment.ts
@@ -1,6 +1,6 @@
-import { autoinject, singleton } from 'aurelia-framework';
-import { User } from '../services/user';
-import {MemberGateway} from '../services/gateway';
+import { autoinject, singleton } from "aurelia-framework";
+import { User } from "../services/user";
+import { MemberGateway } from "../services/gateway";
 
 @autoinject
 @singleton()
@@ -8,8 +8,15 @@ export class Experiment {
     val: any;
     user: User;
     api;
-    options=[{name: 'No', value: false}, {name: 'Yes', value: true}];
-    int_options=[{name: 'red', value: 1}, {name: 'green', value: 2}, {name: 'blue', value: 3}];
+    options = [
+        { name: "No", value: false },
+        { name: "Yes", value: true },
+    ];
+    int_options = [
+        { name: "red", value: 1 },
+        { name: "green", value: 2 },
+        { name: "blue", value: 3 },
+    ];
     bool_val = false;
     int_val = 99;
     date_val = "2022-12-08";
@@ -19,15 +26,11 @@ export class Experiment {
     eventSource;
     incomming_messages = [];
     message_content = "";
+    channel = "all";
 
     constructor(user: User, api: MemberGateway) {
         this.user = user;
         this.api = api;
-    }
-
-    created() {
-        this.eventSource = new EventSource('sse/subscribe');
-        this.eventSource.onmessage = this.handle_incoming_message;
     }
 
     handle_data_change(event) {
@@ -35,14 +38,24 @@ export class Experiment {
         console.log("detail in experiment: ", detail);
     }
 
+    created() {
+        this.eventSource = new EventSource(
+            `sse/subscribe?channel=${this.channel}`
+        );
+        this.eventSource.onmessage = this.handle_incoming_message;
+    }
+
     tease_server() {
-        this.api.call_server_post('sse/tease', {data: this.message_content})
-        .then(response => this.message_content = "");
+        this.api
+            .call_server_post("sse/tease", {
+                data: this.message_content,
+                channel: this.channel,
+            })
+            .then((response) => (this.message_content = ""));
     }
 
     handle_incoming_message(event) {
         this.incomming_messages.push(event.data);
         console.log("message received ", event.data);
     }
-
 }
