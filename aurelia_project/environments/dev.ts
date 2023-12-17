cd6d692977da17951522086caebe4aa4aa8537e0
haimavni <haimavn@gmail.com>
2023-12-17 15:17:53 +0200

    dev is demo

--- aurelia_project/environments/dev.ts
+++ aurelia_project/environments/dev.ts
@@ -1,8 +1,8 @@
 export default {
     debug: true,
     testing: false,
-    baseURL: "https://tol.life",
-    app: 'gbs__master',
+    baseURL: "localhost:8000",
+    app: 'demo',
     version: "",
     i18n_ver: '',
     push_state: false
